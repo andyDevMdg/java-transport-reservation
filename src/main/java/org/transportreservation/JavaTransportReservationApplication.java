c9a303775b1859fcd5179559bcfcefd41bda934c
andyDevMdg <andyavotiana@gmail.com>
2023-08-23 19:18:19 +0300

    initialize the project

---
+++ src/main/java/org/transportreservation/JavaTransportReservationApplication.java
@@ -0,0 +1,13 @@
+package org.transportreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTransportReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTransportReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/transportreservation/JavaTransportReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.transportreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTransportReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
