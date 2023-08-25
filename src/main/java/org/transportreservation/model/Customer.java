c9e005e6946e2ad5b2e848a6a54b70d8493c9530
andyDevMdg <andyavotiana@gmail.com>
2023-08-26 00:52:54 +0300

    feat: add more attributs to Customer

--- src/main/java/org/transportreservation/model/Customer.java
+++ src/main/java/org/transportreservation/model/Customer.java
@@ -2,6 +2,8 @@ package org.transportreservation.model;
 
 import lombok.*;
 
+import java.sql.Timestamp;
+
 
 @Getter
 @Setter
@@ -13,7 +15,10 @@ public class Customer {
     private Long id_customer;
     private String customer_firstname;
     private String customer_lastname;
+    private String customer_username;
+    private String customer_password;
     private String customer_address;
     private double customer_national_id;
     private String customer_mobile_number;
+    private Timestamp customer_registration_date;
 }
