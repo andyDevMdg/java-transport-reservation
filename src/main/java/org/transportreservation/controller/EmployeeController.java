ee630c39476f0135c80367caf87af037884220e4
andyDevMdg <andyavotiana@gmail.com>
2023-08-28 21:32:25 +0300

    feat: add a @GetMapping method to handle the request to get all Employee

--- src/main/java/org/transportreservation/controller/EmployeeController.java
+++ src/main/java/org/transportreservation/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package org.transportreservation.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.transportreservation.model.Employee;
 import org.transportreservation.service.EmployeeService;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -21,6 +23,9 @@ public class EmployeeController {
         return employeeService.insertEmployee(employee);
     }
 
-
+    @GetMapping("/employees")
+    public List<Employee> findAllEmployee() throws SQLException {
+        return employeeService.getAll();
+    }
 
 }
