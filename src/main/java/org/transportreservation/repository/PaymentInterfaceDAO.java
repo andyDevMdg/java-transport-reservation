44134ee2cebb59e45d6fc5a033307282f45cc3ad
andyDevMdg <andyavotiana@gmail.com>
2023-08-28 22:40:03 +0300

    feat: implement all methods in PaymentInterfaceDAO for data access

--- src/main/java/org/transportreservation/repository/PaymentInterfaceDAO.java
+++ src/main/java/org/transportreservation/repository/PaymentInterfaceDAO.java
@@ -1,4 +1,17 @@
 package org.transportreservation.repository;
 
+import org.transportreservation.model.Payment;
+
+import java.util.List;
+
 public interface PaymentInterfaceDAO {
+    void insert(Payment payment);
+
+    List<Payment> getAll();
+
+    Payment getById(int id);
+
+    Payment updateAmountById(int id, double Amount);
+
+    void deleteById(int id);
 }
