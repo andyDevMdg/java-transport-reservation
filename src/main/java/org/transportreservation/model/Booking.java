94d2ae3915e050595b15ace36c11684e9fd53f30
andyDevMdg <andyavotiana@gmail.com>
2023-08-28 21:53:39 +0300

    feat: create Booking  model and its attributs

---
+++ src/main/java/org/transportreservation/model/Booking.java
@@ -0,0 +1,25 @@
+package org.transportreservation.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class Booking {
+    private int id_booking;
+    private String departure_location;
+    private LocalDateTime departure_datetime;
+    private String destination_location;
+    private LocalDateTime destination_datetime;
+    private boolean is_paid;
+    private int id_customer;
+    private int id_payment;
+    private int id_bus_seat;
+    private int id_employee;
+}
