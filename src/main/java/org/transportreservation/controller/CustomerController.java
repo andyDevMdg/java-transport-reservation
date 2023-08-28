fc81fe369e6e8cb4284907e94cff515a17ca363c
andyDevMdg <andyavotiana@gmail.com>
2023-08-28 18:16:21 +0300

    feat: add a @PostMapping method to handle the request to insertCustomer

--- src/main/java/org/transportreservation/controller/CustomerController.java
+++ src/main/java/org/transportreservation/controller/CustomerController.java
@@ -1,4 +1,25 @@
 package org.transportreservation.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.transportreservation.model.Customer;
+import org.transportreservation.service.CustomerService;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
 public class CustomerController {
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer) throws SQLException {
+        return customerService.insertCustomer(customer);
+    }
+
+
+
 }
