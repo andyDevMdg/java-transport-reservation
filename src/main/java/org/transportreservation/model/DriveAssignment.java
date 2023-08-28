275b8610b269c2df60ee708f7eb89911b6c72932
andyDevMdg <andyavotiana@gmail.com>
2023-08-28 21:55:28 +0300

    feat: create DriveAssignment model and its attributs

---
+++ src/main/java/org/transportreservation/model/DriveAssignment.java
@@ -0,0 +1,16 @@
+package org.transportreservation.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class DriveAssignment {
+    private int id_drive_assignment;
+    private int id_bus;
+    private int id_drive;
+}
