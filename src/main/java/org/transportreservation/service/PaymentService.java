0cc294f484103a6ee53f221cde7271886ad5aa82
andyDevMdg <andyavotiana@gmail.com>
2023-08-30 20:51:46 +0300

    feat: add all methods from PaymentDAO to PaymentService

---
+++ src/main/java/org/transportreservation/service/PaymentService.java
@@ -0,0 +1,36 @@
+package org.transportreservation.service;
+
+import org.transportreservation.model.Payment;
+import org.transportreservation.repository.PaymentDAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class PaymentService {
+    private PaymentDAO paymentDAO;
+
+    public PaymentService(PaymentDAO paymentDAO) {
+        this.paymentDAO = paymentDAO;
+    }
+
+    public Payment insertPayment(Payment payment) throws SQLException {
+        paymentDAO.insert(payment);
+        return payment;
+    }
+
+    public List<Payment> getAll() throws SQLException {
+        return paymentDAO.getAll();
+    }
+
+    public Payment getById(int id) throws SQLException {
+        return paymentDAO.getById(id);
+    }
+
+    public Payment updateAmountById(int id, double amount) throws SQLException {
+        return paymentDAO.updateAmountById(id, amount);
+    }
+
+    public void deleteById(int id) throws SQLException {
+        paymentDAO.deleteById(id);
+    }
+}
